9ebec1175c6efe5d7ea976b78a5240b720028a43
Blessing <41972465+Oyelamin@users.noreply.github.com>
2025-11-28 23:29:47 +0100

    Student insights implementation

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+from students import students
+from utils import helpers
 
 def app():
-    print("Printing from app...")
+    helpers.add_student(students, "Praises", 30)
+    highest_score = helpers.highest_score(students)
+    lowest_score = helpers.lowest_score(students)
+    average_score = helpers.average_score(students)
+
+    print("The highest score is", highest_score, "\n")
+    print("The lowest score is", lowest_score, "\n")
+    print("The average score is", average_score, "\n")
+    print("The logged students: ", students, "\n")
 
 
 if __name__ == '__main__':
     print("Starting app....")
+    app()
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,28 @@
 
-def highest_score():
-    print("Print highest score...")
+def highest_score(students) -> int:
+    highest_score = students[0]['score']
+    for student in students:
+        if student['score'] > highest_score:
+            highest_score = student['score']
+    
+    return highest_score        
+    
 
-def lowest_score():
-    print("Print lowest score...")
+def lowest_score(students):
+    lowest_score = students[0]['score']
+    for student in students:
+        if student['score'] < lowest_score:
+            lowest_score = student['score']
+    
+    return lowest_score
+    
+def average_score(data):
+    total = 0
+    for d in data:
+        total += d["score"]
+    return total / len(data)
+
+def add_student(data, name, score):
+    data.append({"name": name, "score": score})
+    
+
